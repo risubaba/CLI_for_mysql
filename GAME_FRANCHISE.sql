908793d01350c8e5d162656d410b6829b35fc6be
Shanmukh Karra <shanmukh1608@gmail.com>
2019-11-12 00:22:45 +0530

    Added few tables, check NOTES at top

--- GAME_FRANCHISE.sql
+++ GAME_FRANCHISE.sql
@@ -1,4 +1,8 @@
 
+-- NOTES:
+-- In 1:M relationship we can add FK to M side
+-- Issue is in M:1 relationship where FK has been added to 1 side in SRS
+
 -- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
 --
 -- Host: localhost    Database: Company
@@ -33,8 +37,17 @@ CREATE TABLE `employee` (
   `address_line1` varchar(100) NOT NULL,
   `address_line2` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`employee_id`)
-  -- KEY `Mgr_ssn` (`Mgr_ssn`),
-  -- CONSTRAINT `DEPARTMENT_ibfk_1` FOREIGN KEY (`Mgr_ssn`) REFERENCES `EMPLOYEE` (`Ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `phone_num_emp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phone_num_emp` (
+  `employee_id` int(11) NOT NULL,
+  `phone_num` int(10) NOT NULL,
+  CONSTRAINT 'compkey_phone_num_emp' PRIMARY KEY (employee_id, phone_num)
+  CONSTRAINT `assigned_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,8 +65,37 @@ CREATE TABLE `customer` (
   `employee_id` int(11) NOT NULL,
   PRIMARY KEY (`customer_id`),
   CONSTRAINT `assigned_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`)
-  -- KEY `Mgr_ssn` (`Mgr_ssn`),
-  -- CONSTRAINT `DEPARTMENT_ibfk_1` FOREIGN KEY (`Mgr_ssn`) REFERENCES `EMPLOYEE` (`Ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `phone_num_cus` (
+  `customer_id` int(11) NOT NULL,
+  `phone_num` int(10) NOT NULL,
+  CONSTRAINT 'compkey_phone_num_cus' PRIMARY KEY (customer_id, phone_num)
+  CONSTRAINT `assigned_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `num_in_stock` int(4) NOT NULL,
+  `price` int(10) NOT NULL,
+  PRIMARY KEY (`product_id`),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+  `store_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `website_address` varchar(50) NOT NULL,
+  `customer_service_website_address` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  PRIMARY KEY (`store_id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
